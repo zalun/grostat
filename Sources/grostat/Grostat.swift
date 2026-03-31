d084da46cfcc3343565fc62c40cff421c1355b4d
Piotr Zalewa <zalun@zalunPrivMBP.local>
2026-03-31 16:18:30 +0200

    Bump version to 0.6.0

--- Sources/grostat/Grostat.swift
+++ Sources/grostat/Grostat.swift
@@ -5,7 +5,7 @@ struct Grostat: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "grostat",
         abstract: "Growatt inverter data collector",
-        version: "0.5.2",
+        version: "0.6.0",
         subcommands: [
             InitCommand.self,
             CollectCommand.self,
